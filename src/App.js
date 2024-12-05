e9c233e7bdc4bec50359ca53cdf63ef0a3b5e242
Catherine <catherine.dieval@hotmail.fr>
2024-12-05 15:45:03 +0100

    Made small fixes to App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function Article() {
   return (
     <article className="article">
       <h2 className="article__title">Hello, I use React</h2>
-      <label htmlFor="input">html</label>
-      <input type="radio" id="html" name="language" value="HTML"></input>
-      <a href="https://en.wikipedia.org/wiki/Pterophyllum">Angelfish</a>
+      <label htmlFor="test">Enter some text:</label>
+      <input type="text" id="test"></input>
+      <a href="https://en.wikipedia.org/wiki/Pterophyllum" target="_blank" rel="noreferrer">Angelfish link on Wikipedia</a>
     </article>
   );
 }
